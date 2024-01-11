7476cc90d07e13436c7ffc35ecd1559456cea5fb
saulo <saulo.miranda19@gmail.com>
2024-01-10 22:16:56 -0300

    primeiro projeto criado

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index(){
+            return View();
+        }
+    }
+}
