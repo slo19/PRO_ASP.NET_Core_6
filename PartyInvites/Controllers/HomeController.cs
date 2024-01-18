ebac1d8771004398c61862cd2c673c0c1be11abd
saulo-miranda <saulo.miranda@ufv.br>
2024-01-17 22:25:53 -0300

    add validação

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -14,8 +14,12 @@ namespace PartyInvites.Controllers {
 
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse) {
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid){
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                return View();
+            }
         }
 
         public ViewResult ListResponses() {
--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PartyInvites.Models {
     public class GuestResponse {
+        [Required(ErrorMessage = "Please enter your name")]
         public string? Name {get; set;}
+        [Required(ErrorMessage = "Please enter your email address")]
         public string? Email {get; set;}
+        [Required(ErrorMessage = "Please enter your phone")]
         public string? Phone {get; set;}
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
         public bool? WillAttend {get; set;}
     }
 }
