c3c495a1afe7401522cf28243242a9c82ffa6af1
saulo-miranda <saulo.miranda@ufv.br>
2024-01-20 19:36:18 -0300

    pattern matching com is

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -5,8 +5,14 @@ namespace LanguageFeatures.Controllers {
     public class HomeController : Controller {
 
         public ViewResult Index() {
-            Product?[] products = Product.GetProducts();
-            return View(new string[] {products[1]!.Name});
+            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10};
+            decimal total = 0;
+            for (int i = 0; i < data.Length; i++) {
+                if (data[i] is decimal d){
+                    total += d;
+                }
+            }
+            return View("Index", new string[] { $"Total: {total:C2}"});
         }
     }
 }
