1e9bf26fd0e59c65b6decc19a078d45a13f321cb
saulo-miranda <saulo.miranda@ufv.br>
2024-01-19 23:18:13 -0300

    add globalUsings

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc;
+//using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers {
     public class HomeController : Controller {
 
         public ViewResult Index() {
-            return View(new string[] {"C#", "Language", "Features"});
+            Product[] products = Product.GetProducts();
+            return View(new string[] { products[0].Name});
         }
     }
 }
