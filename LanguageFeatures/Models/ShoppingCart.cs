8b7ac3958a22a56cbbd29d21f65ddae50504dbb5
saulo-miranda <saulo.miranda@ufv.br>
2024-01-20 21:00:24 -0300

    extensão de interface

--- LanguageFeatures/Models/ShoppingCart.cs
+++ LanguageFeatures/Models/ShoppingCart.cs
@@ -1,5 +1,11 @@
+using System.Collections;
+
 namespace LanguageFeatures.Models {
-    public class ShoppingCart {
+    public class ShoppingCart : IEnumerable<Product?> {
         public IEnumerable<Product?>? Products {get; set;}
+
+        public IEnumerator<Product?> GetEnumerator() => Products?.GetEnumerator() ?? Enumerable.Empty<Product?>().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
