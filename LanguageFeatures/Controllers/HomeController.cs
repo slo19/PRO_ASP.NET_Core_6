b28d8890ebd5bf1b1e15e39ca255442bff90336b
saulo-miranda <saulo.miranda@ufv.br>
2024-01-23 20:29:49 -0300

    usando funções lambda para filtrar

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -4,9 +4,10 @@
 namespace LanguageFeatures.Controllers {
     public class HomeController : Controller {
 
-        bool FilterByPrice(Product? p) {
+        /*bool FilterByPrice(Product? p) {
             return (p?.Price ?? 0) >= 20;
         }
+        */
         public ViewResult Index() {
             ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts()};
 
@@ -17,12 +18,13 @@ namespace LanguageFeatures.Controllers {
                 new Product {Name="Corner flag", Price=34.95M}
             };
 
-            Func<Product?, bool> nameFilter = delegate (Product? prod) {
+            /*Func<Product?, bool> nameFilter = delegate (Product? prod) {
                 return prod?.Name[0] == 'S';
             };
+            */
 
-            decimal priceFilterTotal = productArray.Filter(FilterByPrice).TotalPrices();
-            decimal nameFilterTotal = productArray.Filter(nameFilter).TotalPrices();
+            decimal priceFilterTotal = productArray.Filter(p => (p?.Price ?? 0) >= 20).TotalPrices();
+            decimal nameFilterTotal = productArray.Filter(p => p?.Name?[0] == 'S').TotalPrices();
             return View("Index", new string[] { $"Price Total: {priceFilterTotal:C2}", $"Name Total: {nameFilterTotal:C2}"});
         }
     }
