f65c4b18252e690f0bba0f763dc8d0cef8170b24
saulo-miranda <saulo.miranda@ufv.br>
2024-01-22 23:27:46 -0300

    adicionando extensões para filtro

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -9,12 +9,14 @@ namespace LanguageFeatures.Controllers {
 
             Product[] productArray = {
                 new Product {Name="Kayak", Price=275M},
-                new Product {Name="Lifejacket", Price=48.95M}
+                new Product {Name="Lifejacket", Price=48.95M},
+                new Product {Name="Soccer ball", Price=19.50M},
+                new Product {Name="Corner flag", Price=34.95M}
             };
 
-            decimal cartTotal = cart.TotalPrices();
-            decimal arrayTotal = productArray.TotalPrices();
-            return View("Index", new string[] { $"Cart Total: {cartTotal:C2}", $"Array Total: {arrayTotal:C2}"});
+            decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
+            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+            return View("Index", new string[] { $"Price Total: {priceFilterTotal:C2}", $"Name Total: {nameFilterTotal:C2}"});
         }
     }
 }
--- LanguageFeatures/Models/MyExtensionMethods.cs
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -16,5 +16,15 @@ namespace LanguageFeatures.Models {
                 }
             }
         }
+
+        public static IEnumerable<Product?> FilterByName(
+            this IEnumerable<Product?> productEnum, char firstLetter
+        ) {
+            foreach (Product? prod in productEnum) {
+                if (prod?.Name?[0] == firstLetter) {
+                    yield return prod;
+                }
+            }
+        }
     }
 }
